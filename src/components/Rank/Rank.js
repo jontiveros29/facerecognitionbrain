b4fcc6a9e51d0a5d5b571d376256dbfb770e4552
Ontiveros <jontiveros@c02fl5bemd6r.hsd1.fl.comcast.net>
2021-07-28 23:52:50 -0400

    Face

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+     <div>
+       <div className='black f3'>
+            {'Jose, your current rank is...'}
+       </div>
+       <div className='black f1'>
+            {'#4'}
+       </div>
+     </div>
+    );
+}
+
+export default Rank;
